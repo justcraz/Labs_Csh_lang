1c5ee1826d2f899238b00b23266af98df965877c
Alxich <76397116+Alxich@users.noreply.github.com>
2022-03-12 15:48:07 +0200

    Added new lab

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1 {
+    public class Program {
+
+        static public int Example(int a, int b) {
+            int c = a + b;
+            Console.WriteLine(a.ToString() + " + " + b.ToString() + " = " + c.ToString());
+
+            return c;
+        }
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("Write yout first number");
+            string S1 = Console.ReadLine();
+
+            int a = int.Parse(S1);
+            Console.WriteLine("Write yout second number");
+
+            string S2 = Console.ReadLine();
+
+            int b = int.Parse(S2);
+            Example(a, b);
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
